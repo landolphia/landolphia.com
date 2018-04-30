2493ebdf8a1f0776ccf6cc041c1efd7dfe3b3bc8
Landolphia <landolphia@users.noreply.github.com>
2018-04-30 10:16:54 -0400

    UI refactoring for templating

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+
+import '../imports/ui/components/title-bar.css';
+import '../imports/ui/components/title-bar.html';
+import '../imports/ui/components/site-navigation.css';
+import '../imports/ui/components/site-navigation.html';
+import '../imports/ui/components/project-navigation.css';
+import '../imports/ui/components/project-navigation.html';
+import '../imports/ui/components/project-content.css';
+import '../imports/ui/components/project-content.html';
+import '../imports/ui/layouts/main.css';
+import '../imports/ui/layouts/main.html';
